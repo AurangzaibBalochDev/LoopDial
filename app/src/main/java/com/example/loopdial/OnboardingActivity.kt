679bb33f00eadc5fb0c84fd325cc96422eb9b3a2
Hina Tariq <hina.tariq@wifi-connect.eu>
2025-10-03 23:35:57 +0500

    initial commit

---
+++ app/src/main/java/com/example/loopdial/OnboardingActivity.kt
@@ -0,0 +1,26 @@
+package com.example.loopdial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loopdial.databinding.ActivityOnboardingBinding
+
+class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = OnboardingPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+
+        // Attach dots indicator
+        binding.dotsIndicator.attachTo(binding.viewPager)
+    }
+
+    fun goToPage(index: Int) {
+        binding.viewPager.currentItem = index
+    }
+}
